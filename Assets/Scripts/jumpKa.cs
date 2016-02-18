313b96e4866c5b9a16e439fa40f068f7655f4115
Joseph Jolton <j@jolton.com>
2016-02-17 20:30:31 -0600

    Initial Commit

---
+++ Assets/Scripts/jumpKa.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class jumpKa : MonoBehaviour
+{
+	public int jumpHeight;
+	private Rigidbody rigid;
+	private AudioSource giggle;
+
+	// Use this for initialization
+	void Start ()
+	{
+		rigid = GetComponent<Rigidbody> ();
+		giggle = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetButtonDown ("Jump")) {
+			GetComponent<Animation> ().Play ();
+		}
+	
+	}
+
+	void jumpUp ()
+	{
+		rigid.AddForce (Vector3.up * jumpHeight);
+		giggle.Play ();
+
+	}
+}
---
+++ Assets/Scripts/movement.cs
@@ -0,0 +1,58 @@
+﻿//using UnityEngine;
+//using System.Collections;
+//
+//public class movement : MonoBehaviour
+//{
+//
+//	public float forceSpeed;
+//	public float jumpHeight;
+//	public Rigidbody rigid;
+//	bool grouxnded;
+//
+//	float horiMove;
+//	float jumpMove;
+//
+//	// Use this for initialization
+//	void Start ()
+//	{
+//		if (GetComponent<Rigidbody> () != null) {
+//			return;
+//		} else {
+//			Debug.Log ("This GameObject needs a rigidbody");
+//		}
+//		rigid = GetComponent<Rigidbody> ();
+//	}
+//	
+//	// Update is called once per frame
+//	void FixedUpdate ()
+//	{
+//		horiMove = Input.GetAxis ("Mouse X") * forceSpeed;
+//		jumpMove = Input.GetAxis ("Jump") * jumpHeight;
+//		rigid.AddForce (horiMove, jumpMove, 0);
+//
+//	}
+//
+//	void OnCollisionEnter (Collision col)
+//	{
+//		if (col.gameObject.tag == "Ground") {
+//			grounded = true;
+//		}
+//		Debug.Log (grounded);
+//	}
+//
+//	void OnCollisionStay (Collision col)
+//	{
+//		if (col.gameObject.tag == "Ground") {
+//			grounded = true;
+//		}
+//		Debug.Log (grounded);
+//	}
+//
+//	void OnCollisionExit (Collision col)
+//	{
+//		if (col.gameObject.tag == "Ground") {
+//			grounded = false;
+//		}
+//		Debug.Log (grounded);
+//	}
+//}

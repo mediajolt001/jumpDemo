c9b9bbcf985ce045f43f097c8dfd0bd6cb89cb8e
Joseph Jolton <j@jolton.com>
2016-04-07 18:20:58 -0500

    A bunch of bullshit assets I don't really need.

---
+++ Assets/Scripts/rollKube.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rollKube : MonoBehaviour
+{
+
+	public int kubeSpeed;
+	float horiMove;
+	Rigidbody rigid;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		rigid = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		horiMove = Input.GetAxis ("Horizontal") * -kubeSpeed;
+		rigid.AddTorque (0, 0, horiMove);
+	}
+}
+
